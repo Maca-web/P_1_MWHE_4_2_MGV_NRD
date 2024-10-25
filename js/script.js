f7b7e10382c0be93f105a7131fdfaba078790837
nikitaaaray <nikitarayskayaya@gmail.com>
2024-10-25 17:57:40 +0200

    Actualización BRANDS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 
 // FLIPPING CARDS 
+
+
+// BRANDS
+$(document).ready(function() {
+    $('.brand-title').click(function() {
+        var description = $(this).next('.brand-description');
+        
+        // Ocultar el h2
+        $(this).fadeOut(function() {
+            // Al ocultar el h2, mostrar el p
+            description.slideDown(); // Muestra el p con efecto de deslizamiento
+        });
+    });
+});
