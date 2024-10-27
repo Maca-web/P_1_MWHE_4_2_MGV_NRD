2bad16944da5e8d08ce4b682ba67a592da761b37
nikitaaaray <nikitarayskayaya@gmail.com>
2024-10-27 19:16:17 +0100

    BRANDS + CARDS

--- js/script.js
+++ js/script.js
@@ -3,14 +3,41 @@
 
 
 // BRANDS
+
 $(document).ready(function() {
     $('.brand-title').click(function() {
-        var description = $(this).next('.brand-description');
-        
-        // Ocultar el h2
-        $(this).fadeOut(function() {
-            // Al ocultar el h2, mostrar el p
-            description.slideDown(); // Muestra el p con efecto de deslizamiento
+        var title = $(this); 
+        var description = title.next('.brand-description');
+
+        title.fadeOut(function() {
+            description.css("display", "block").animate({ opacity: 1 }, 500); // Muestra y desvanece la descripción
+        });
+
+        description.click(function() {
+            description.animate({ opacity: 0 }, 500, function() { // Reduce la opacidad gradualmente
+                description.css("display", "none");
+                title.fadeIn(); // Muestra el título después de la animación
+            });
         });
     });
+
+    // Variable para el temporizador de scroll
+    let timeoutId;
+
+    $(window).scroll(function() {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(function() {
+            $('.brand-description').animate({ opacity: 0 }, 500, function() { // Desvanece la descripción en scroll
+                $(this).css("display", "none");
+                $('.brand-title').fadeIn();
+            });
+        }, 300); // Retraso antes de restablecer
+    });
+
+
+
+
+    // IMÁGENES CARDS 
+    
 });
