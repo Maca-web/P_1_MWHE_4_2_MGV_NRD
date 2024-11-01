2b3a270fdb6307f6444b83b2dd9881a7bf80e15a
Maca-web <macarenamgva@gmail.com>
2024-11-01 11:18:49 +0100

    countdown

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+// COUNTDOWN
+$(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    // Actualización automática de la fecha del próximo año si es necesario
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "09/30/",
+        birthday = dayMonth + yyyy;
+  
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    // Fin de actualización
+  
+    const countDown = new Date(birthday).getTime();
+    const x = setInterval(function() {    
+  
+      const now = new Date().getTime(),
+            distance = countDown - now;
+  
+      $("#days").text(Math.floor(distance / day));
+      $("#hours").text(Math.floor((distance % day) / hour));
+      $("#minutes").text(Math.floor((distance % hour) / minute));
+      $("#seconds").text(Math.floor((distance % minute) / second));
+  
+      // Cuando se alcance la fecha
+      if (distance < 0) {
+        $("#headline").text("¡Es mi cumpleaños!");
+        $("#countdown").hide();
+        $("#content").show();
+        clearInterval(x);
+      }
+      // Tiempo de actualización en milisegundos
+    }, 0);
+  });
+  
 
 // FLIPPING CARDS 
 
